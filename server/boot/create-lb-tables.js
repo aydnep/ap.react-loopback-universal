dfb3d8e91cf6835f5ba572fe1c307e3ef012dcd9
Andrii Pindiura <aydnep@aydnep.com.ua>
2016-11-09 16:02:57 +0200

    change test datasource to json file & fix error

--- server/boot/create-lb-tables.js
+++ server/boot/create-lb-tables.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
   // console.log(app.models()[0]);
-  const ds = app.dataSources.mysql;
+  const ds = app.dataSources.db;
   const lbTables = ['user', 'accessToken', 'userCredential', 'userIdentity', 'ACL', 'RoleMapping', 'Role'];
   // const lbTables = ['ACL', 'RoleMapping', 'Role'];
   // const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
@@ -24,13 +24,8 @@ module.exports = (app) => {
         username: 'test',
       }, (findErr, instance, created) => {
         if (findErr) {
-          throw findErr;
-        }
-        // if (instance) {
-        //   console.log(instance);
-        //   console.log('test user exist');
-        // }
-        if (created) {
+          console.log(findErr);
+        } else if (created) {
           console.log(created);
           console.log('Create test user');
         }
